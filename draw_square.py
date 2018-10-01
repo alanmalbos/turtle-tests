e8453d28a01e411ac388db31a4cc6dd268eb9a6d
Alan Malbos <alanmalbos@gmail.com>
2018-10-01 20:51:02 -0300

    Add a new turtle and draw a circle

--- draw_square.py
+++ draw_square.py
@@ -22,6 +22,17 @@ def draw_square():
 	brad.forward(100)
 	brad.right(90)
 
+	# instancing a new turtle
+	bozo = turtle.Turtle()
+
+	# customizing the new turtle
+	bozo.shape("arrow")
+	bozo.color("yellow")
+	bozo.speed(2)
+
+	# drawing a circle
+	bozo.circle(100)
+
 	window.exitonclick()
 
 draw_square()
