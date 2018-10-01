4650631640009d4802e050caf1bce39d60d6dee8
Alan Malbos <alanmalbos@gmail.com>
2018-10-01 20:43:00 -0300

    Add a customize turtle in square file

--- draw_square.py
+++ draw_square.py
@@ -4,7 +4,15 @@ def draw_square():
 	window = turtle.Screen()
 	window.bgcolor("red")
 
+	# instancing a turtle to draw
 	brad = turtle.Turtle()
+
+	# customizing the turtle
+	brad.shape("turtle")
+	brad.color("blue")
+	brad.speed(2)
+
+	# drawing a square
 	brad.forward(100)
 	brad.right(90)
 	brad.forward(100)
